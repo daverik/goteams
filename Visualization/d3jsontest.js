6066e47f699377e3a6282e12ffdf85d0d712c807
viltord <viktorswantesson@gmail.com>
2013-10-14 15:57:25 +0200

    Trying the visualization

--- Visualization/d3jsontest.js
+++ Visualization/d3jsontest.js
@@ -2,6 +2,7 @@ d3json();
 
 function d3json ()
 {
+	/*
 	var canvas = d3.select("body")
 		.append("svg")
 		.attr("width", 1000)
@@ -48,61 +49,70 @@ function d3json ()
 				.duration(3000);
 					
 	});
-	
+	*/
 	///////////////////////////////////////////////////////////////
 	
 	var points = [
-  [480, 200],
-  [580, 400],
-  [680, 100],
-  [780, 300],
-  [180, 300],
-  [280, 100],
-  [380, 400]
-];
-
-var svg = d3.select("body").append("svg")
-    .attr("width", 960)
-    .attr("height", 500);
-
-var path = svg.append("path")
-    .data([points])
-    .attr("fill", "transparent")
-    .attr("stroke", "black")
-    .attr("stroke-width", 5)
-    .attr("d", d3.svg.line()
-    .tension(0) // Catmull–Rom
-    .interpolate("cardinal-closed"));
-
-svg.selectAll(".point")
-    .data(points)
-  .enter().append("circle")
-    .attr("r", 8)
-    .attr("transform", function(d) { return "translate(" + d + ")"; });
-
-var circle = svg.append("circle")
-    .attr("r", 13)
-    .attr("fill", "red")
-    .attr("transform", "translate(" + points[0] + ")");
-
-transition();
-
-function transition() {
-  circle.transition()
-      .duration(10000)
-      .attrTween("transform", translateAlong(path.node()))
-      .each("end", transition);
-}
-
-// Returns an attrTween for translating along the specified path element.
-function translateAlong(path) {
-  var l = path.getTotalLength();
-  return function(d, i, a) {
-    return function(t) {
-      var p = path.getPointAtLength(t * l);
-      return "translate(" + p.x + "," + p.y + ")";
-    };
-  };
+	  [480, 200],
+	  [580, 400],
+	  [680, 100],
+	  [780, 300],
+	  [180, 300],
+	  [280, 100],
+	  [380, 400]
+	];
+	
+	var svg = d3.select("body").append("svg")
+	    .attr("width", 960)
+	    .attr("height", 500);
+	
+	var path = svg.append("path")
+	    .data([points])
+	    .attr("fill", "transparent")
+	    .attr("stroke", "black")
+	    .attr("stroke-width", 5)
+	    .attr("d", d3.svg.line()
+		    .tension(0) // Catmull–Rom
+		    .interpolate("cardinal-closed"));
+	
+	svg.selectAll(".point")
+	    .data(points)
+	  .enter().append("circle")
+	    .attr("r", 8)
+	    .attr("transform", function(d) { return "translate(" + d + ")"; });
+	
+	var circle2 = svg.append("circle")
+	    .attr("r", 13)
+	    .attr("fill", "green")
+	    .attr("transform", "translate(" + points[0] + ")");
+	
+	
+	var circle1 = svg.append("circle")
+	    .attr("r", 13)
+	    .attr("fill", "red")
+	    .attr("transform", "translate(" + points[0] + ")");
+	
+	
+	transition(circle1);
+	
+	//setTimeout(transition(circle2), 1000);
+	
+	function transition(circle) {
+	  circle.transition()
+	      .duration(10000) //Millisekunder
+	      .attrTween("transform", translateAlong(path.node()))
+	      .each("end", transition);
+	}
+	
+	// Returns an attrTween for translating along the specified path element.
+	function translateAlong(path) {
+	  var l = path.getTotalLength();
+	  return function(d, i, a) {
+	    return function(t) {
+	      var p = path.getPointAtLength(t * l);
+	      return "translate(" + p.x + "," + p.y + ")";
+	    };
+	  };
 }
 		
 /////////////////////////////////////////////////////			
