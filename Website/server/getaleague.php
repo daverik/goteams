50002bd79b7a23787edd39f1fe37abb7153a6f42
David <Daven@dhcp-194228.eduroam.chalmers.se>
2013-10-16 10:46:38 +0200

    Can press enter on login

---
+++ Website/server/getaleague.php
@@ -0,0 +1,6 @@
+<?php
+	if($_POST['league'] == 'Chalmers-ligan') {
+		echo '{ "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus pharetra leo nisi. Etiam luctus semper dolor ac dignissim. Aenean suscipit vehicula arcu, eget suscipit massa venenatis sit amet. Nulla non lorem id mauris feugiat dictum eget in nisl. ",
+		"teams": ["IxD", "Software Engineering"]}';	
+	}
+?>
