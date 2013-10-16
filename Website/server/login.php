5ed74fdf033e4450e7fb39201519e258671c7904
Victor Karlsson <victorc@student.chalmers.se>
2013-10-16 09:57:12 +0200

    Logging in with a user

--- Website/server/login.php
+++ Website/server/login.php
@@ -1,11 +1,27 @@
 <?php
-	// Test push from Victor
-	// Test push 2 3
-	// TEST AGAIN
-	if($_POST['name'] == '') {
-		echo '{ "loginSuccess": true }';	
-	} else {
-		echo '{ "loginSuccess": false }';
-	}
-	
+
+    session_start();
+    $dbcnx = @mysql_connect("localhost", "root", "");
+ 	
+	// if not connected, login anyway (for testing purposes)
+ 	if (!$dbcnx) {
+        echo '{ "loginSuccess": true }';
+        exit();
+    }
+    mysql_select_db("lagvarvet", $dbcnx);
+
+    $enteredUsername = $_POST['name'];
+
+    $query = mysql_query("SELECT username FROM runner WHERE username = '$enteredUsername'");
+    while ( $row = mysql_fetch_array($query) ) {
+        $usrname = $row["username"];
+    }
+
+    if($_POST['name'] == $usrname) {
+    	$_SESSION['username'] = $usrname;
+        echo '{ "loginSuccess": true }';
+    } else {
+        echo '{ "loginSuccess": false }';
+    }
+
 ?>
