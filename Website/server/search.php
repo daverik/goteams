9375c54bd08baac0cc16260244408d39244cddb2
Victor Karlsson <victorc@student.chalmers.se>
2013-10-18 10:07:43 +0200

    Search implemented

--- Website/server/search.php
+++ Website/server/search.php
@@ -1,12 +1,26 @@
 <?php
+	$dbcnx = @mysql_connect("localhost", "root", "root");
+    mysql_select_db("lagvarvet", $dbcnx);
+    mysql_query ('SET NAMES UTF8;');
+    mysql_query ('SET COLLATION_CONNECTION=utf8_general_ci;');
+    mysql_client_encoding($dbcnx);
+	
 	$value = $_POST['value'];
+    
+    $qTeams = mysql_query("SELECT title FROM team");
+   
+    $str = '{"values":[';
+    while($row = mysql_fetch_array($qTeams)) {
+        $team = $row["title"];
+        $str .= '"'.$team.'",';
+    }
+    $str = substr($str,0,strlen($str)-1);
+    $str .= ']}';
 	
-	$str = '{"values":["apa","apple","bosse"]}';
+    $output = '{"values":[';
 	
-	$output = '{"values":[';
-	
-	if(strlen($value) > 0) {
-       $pattern = '/'.$value.'/';
+    if(strlen($value) > 0) {
+       $pattern = '/^'.$value.'/i';
 	   $json = json_decode($str);
 	   
 	   foreach($json->values as $item) {
@@ -16,7 +30,7 @@
    		}
 	}
        
-	$output = substr($output,0,strlen($output)-1).']}';
-		echo $output;
-	}
+    $output = substr($output,0,strlen($output)-1).']}';
+    echo $output;
+    }
 ?>
